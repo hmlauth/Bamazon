1c0c4b5060092862385d49d11609910add19f202
Andromeda <hannalauth@gmail.com>
2019-03-03 17:15:59 -0800

    mvp logic working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,12 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table");
 
+// CONNECT TO MYSQL
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Your port
   port: 3306,
 
   // Your username
@@ -14,75 +17,85 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  displayProducts();
 });
 
-// function createProduct() {
-//   console.log("Inserting a new product...\n");
-//   var query = connection.query(
-//     "INSERT INTO products SET ?",
-//     {
-//       flavor: "Rocky Road",
-//       price: 3.0,
-//       quantity: 50
-//     },
-//     function(err, res) {
-//       console.log(res.affectedRows + " product inserted!\n");
-//       // Call updateProduct AFTER the INSERT completes
-//       updateProduct();
-//     }
-//   );
+// FUNCTIONS
+// display all items available for sale
+function displayProducts() {
+  console.log("\nShowing current inventory...\n");
+  connection.query(
+    "SELECT * FROM products", 
+    function (err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      promptBuyer()
+  });
+}
 
-  // logs the actual query being run
-//   console.log(query.sql);
-// }
+function promptBuyer() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Would you like to purchase something?",
+      choices: ["Yes", "No"],
+      name: "buyerChoice",
+    }
+  ]).then(function (answer) {
+    if (answer.buyerChoice === "No") {
+      console.log("Thanks for shopping!");
+      console.log("Press CTRL c to exit.")
+    } else if (answer.buyerChoice === "Yes") {
+      inquirer.prompt([
+        {
+          type: "input",
+          message: "Please select the item_id that you would like to purchase.",
+          name: "item_idChoice"
+        },
+        {
+          type: "input",
+          message: "How many units would you like buy of this product?",
+          name: "stock_quantityChoice"
+        }
+      ]).then(function (answer) {
+        console.log("Updating quantities...\n");
+        connection.query(
+          "SELECT stock_quantity FROM products WHERE item_id = ?", 
+          answer.item_idChoice, 
+          function (err, res) {
+            if (err) throw err;
+            var oldStockQuantity = res[0].stock_quantity;
+            var newStockQuantity = oldStockQuantity - answer.stock_quantityChoice;
 
-// function updateProduct() {
-//   console.log("Updating all Rocky Road quantities...\n");
-//   var query = connection.query(
-//     "UPDATE products SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
-
-//   // logs the actual query being run
-//   console.log(query.sql);
-// }
-
-// function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
-//   connection.query(
-//     "DELETE FROM products WHERE ?",
-//     {
-//       flavor: "strawberry"
-//     },
-//     function(err, res) {
-//       console.log(res.affectedRows + " products deleted!\n");
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// }
-
-// function readProducts() {
-//   console.log("Selecting all products...\n");
-//   connection.query("SELECT * FROM products", function(err, res) {
-//     if (err) throw err;
-//     // Log all results of the SELECT statement
-//     console.table(res);
-//     connection.end();
-//   });
-// }
+            if (newStockQuantity < 0) {
+              console.log("Insufficient quantity! There are only " + oldStockQuantity + " units of this product available.");
+              promptBuyer();
+            } else {
+              connection.query(
+                "UPDATE products SET ? WHERE ?", 
+                  [
+                    {stock_quantity: newStockQuantity},
+                    {item_id: answer.item_idChoice}
+                  ], function (err, res) {
+                    connection.query(
+                      "SELECT price FROM products WHERE item_id = ?", 
+                      answer.item_idChoice, 
+                      function (err, res) {
+                        if (err) throw err;
+                        var costOfProduct = res[0].price * answer.stock_quantityChoice;
+                        console.log("\tTransaction complete! \n\tTotal Cost: $" + costOfProduct + "\n");
+                        displayProducts();
+                  }
+                )
+              }
+              )
+            }
+          }
+        );
+      });
+    }
+  })
+}
